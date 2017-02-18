75f7f70e4d1b5e37d112964347178b8cd937a277
Jon Renaut <jrenaut@gmail.com>
2017-02-17 23:16:31 -0500

    yeah you need a password too huh

--- lawgo/settings.py
+++ lawgo/settings.py
@@ -99,6 +99,7 @@ else:
             'ENGINE': os.environ['ENGINE'],
             'NAME': os.environ['NAME'],
             'USER': os.environ['USER'],
+            'PASSWORD': os.environ['PASSWORD'],
             'HOST': os.environ['HOST'],
         }
     }
