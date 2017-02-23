f30deac20fee2b70cca6dd9a49804bb2127caf36
LAWG Admin <lawgadmin@crossbow.dreamhost.com>
2017-02-22 18:07:33 -0800

    working passenger wsgi

---
+++ passenger_wsgi.py
@@ -0,0 +1,18 @@
+#!/home/lawgadmin/awgpodcast.com/env/bin/python
+import sys, os
+INTERP = "/home/lawgadmin/awgpodcast.com/env/bin/python"
+#INTERP is present twice so that the new python interpreter knows the actual executable path
+if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
+
+cwd = os.getcwd()
+sys.path.append(cwd)
+sys.path.append('/home/lawgadmin/awgpodcast.com/learning-as-we-go/')  #You must add your project here
+
+sys.path.insert(0,cwd+'/env/bin')
+sys.path.insert(0,cwd+'/env/lib/python2.7/site-packages/django')
+sys.path.insert(0,cwd+'/env/lib/python2.7/site-packages')
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "lawgo.settings"
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
