36cf1d06910e81dd50d8c485eb4c758038a2747a
Jon Renaut <jrenaut@gmail.com>
2017-02-22 21:12:09 -0500

    add allowed host

--- lawgo/settings.py
+++ lawgo/settings.py
@@ -29,12 +29,12 @@ except:
 SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = ENVIRONMENT == 'PROD'
+DEBUG=True
 if ENVIRONMENT=='DEV':
     ALLOWED_HOSTS = []
 else:
-    ALLOWED_HOSTS = [u'aqueous-basin-33767.herokuapp.com',]
+    ALLOWED_HOSTS = [u'www.awgpodcast.com',]
 
 
 # Application definition
